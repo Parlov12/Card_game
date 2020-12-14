6d6e3a93be50a9761211f3b82b02e2f3c2b4604d
Petar Parlov <petarparlov4@gmail.com>
2020-12-14 17:54:09 +0100

    Repair commit

--- app/src/main/java/com/example/card_game/Blackjack.kt
+++ app/src/main/java/com/example/card_game/Blackjack.kt
@@ -130,7 +130,17 @@ class Blackjack : AppCompatActivity() {
         repeat_bet.setImageResource(0)
 
 
-        var bjNiz: IntArray = intArrayOf(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,50,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51)
+        var bjNiz = IntArray(104){it}
+        var j: Int = 52
+
+        for(i in 0..51)
+        {
+            bjNiz[j] = i
+            bjNiz[i] = i
+            j++
+        }
+
+
         balance_text.text = "$stanje"
 
 
--- app/src/main/java/com/example/card_game/SplashScreen.kt
+++ app/src/main/java/com/example/card_game/SplashScreen.kt
@@ -1,22 +1,50 @@
 package com.example.card_game
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.preference.PreferenceManager
+import android.view.View
 
 
 class SplashScreen : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
         var handler = Handler()
+        val user = Intent(this, UsernameActivtiy::class.java)
+        val main = Intent(this, MainActivity::class.java)
+
+
+        var pref = PreferenceManager.getDefaultSharedPreferences(this)
+        var savedUsername = pref.getString("username","")
+
+
 
         handler.postDelayed({
-            val splash = Intent(this, MainActivity::class.java)
-            startActivity(splash)
-            finish()
+            if(savedUsername == "")
+            {
+                startActivity(user)
+                finish()
+            }
+            else
+            {
+                startActivity(main)
+                finish()
+            }
+
         }, 1000)
+
+
+
+
+
     }
+
 }
